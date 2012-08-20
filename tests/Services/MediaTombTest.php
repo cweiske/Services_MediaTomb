a5d1a0d9b3d944fabd85e988df906ff1d1ddcea4
Christian Weiske <cweiske@cweiske.de>
2012-08-20 18:40:39 +0200

    split unit test initialization off into base class

--- tests/Services/MediaTombTest.php
+++ tests/Services/MediaTombTest.php
@@ -1,74 +1,21 @@
 <?php
+require_once __DIR__ . '/MediaTombTestBase.php';
+
 /**
  * Test class for Services_MediaTomb.
  *
  * @author Christian Weiske <cweiske@cweiske.de>
  */
-class Services_MediaTombTest extends PHPUnit_Framework_TestCase
+class Services_MediaTombTest extends Services_MediaTombTestBase
 {
-    protected $configExists = null;
-
-    /**
-     * @var Services_MediaTomb
-     */
-    protected $smt;
-
-
     public function __construct()
     {
-        $configFile = dirname(__FILE__) . '/../config.php';
-        $this->configExists = file_exists($configFile);
-        if ($this->configExists) {
-            include_once $configFile;
-        }
-
-        require_once 'Services/MediaTomb.php';
-
+        parent::__construct();
         $strMockPath = dirname(__FILE__) . '/MediaTombTest/MediaTombMock.php';
         if (file_exists($strMockPath)) {
             require_once $strMockPath;
         } else {
             require_once 'Services/MediaTombTest/MediaTombMock.php';
-        }
-    }
-
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     *
-     * @access protected
-     */
-    protected function setUp()
-    {
-        if (!$this->configExists) {
-            $this->markTestSkipped('Unit test configuration is missing.');
-        }
-        $this->smt = new Services_MediaTomb(
-            $GLOBALS['Services_MediaTomb_UnittestConfig']['username'],
-            $GLOBALS['Services_MediaTomb_UnittestConfig']['password'],
-            $GLOBALS['Services_MediaTomb_UnittestConfig']['host'],
-            $GLOBALS['Services_MediaTomb_UnittestConfig']['port']
-        );
-        if (isset($GLOBALS['Services_MediaTomb_UnittestConfig']['bWorkaroundTimingBug'])) {
-            $this->smt->bWorkaroundTimingBug
-                = $GLOBALS['Services_MediaTomb_UnittestConfig']['bWorkaroundTimingBug'];
-        }
-    }
-
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     *
-     * @access protected
-     */
-    protected function tearDown()
-    {
-        //remove unittest container
-        if ($this->smt) {
-            $cont = $this->smt->getContainerByPath('unittest');
-            if ($cont instanceof Services_MediaTomb_Container) {
-                $cont->delete();
-            }
         }
     }
 
