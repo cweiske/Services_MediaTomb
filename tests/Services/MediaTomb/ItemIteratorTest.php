573a02dda120db0493c14e4ff17437528d4e0b8f
Christian Weiske <cweiske@cweiske.de>
2012-08-21 07:50:52 +0200

    test 3 items only, add paging test

--- tests/Services/MediaTomb/ItemIteratorTest.php
+++ tests/Services/MediaTomb/ItemIteratorTest.php
@@ -8,13 +8,13 @@ require_once __DIR__ . '/../MediaTombTestBase.php';
  */
 class Services_MediaTomb_ItemIteratorTest extends Services_MediaTombTestBase
 {
-    protected function makeSome()
+    protected function makeSome($nCount = 3)
     {
         $containerPath = 'unittest/iterator/';
         $container = $this->smt->createContainerByPath($containerPath);
         $this->assertInstanceOf('Services_MediaTomb_Container', $container);
 
-        for ($n = 0; $n < 10; $n++) {
+        for ($n = 0; $n < $nCount; $n++) {
             $item = new Services_MediaTomb_Item();
             $item->title       = 'item #' . $n;
             $item->location    = __FILE__;
@@ -34,19 +34,33 @@ class Services_MediaTomb_ItemIteratorTest extends Services_MediaTombTestBase
     public function testIterator()
     {
         $container = $this->makeSome();
-        $iterator = $container->getItemIterator();
+        $iterator  = $container->getItemIterator();
         $count = 0;
         foreach ($iterator as $item) {
             $this->assertInstanceOf('Services_MediaTomb_Item', $item);
+            $this->assertEquals('item #' . $count, $item->title);
             ++$count;
         }
-        $this->assertEquals(10, $count);
+        $this->assertEquals(3, $count);
+    }
+
+    public function testIteratorPaging()
+    {
+        $container = $this->makeSome(5);
+        $iterator  = $container->getItemIterator(false, 2);
+        $count = 0;
+        foreach ($iterator as $item) {
+            $this->assertInstanceOf('Services_MediaTomb_SimpleItem', $item);
+            $this->assertEquals('item #' . $count, $item->title);
+            ++$count;
+        }
+        $this->assertEquals(5, $count);
     }
 
     public function testIteratorCount()
     {
         $container = $this->makeSome();
-        $this->assertEquals(10, iterator_count($container->getItemIterator()));
+        $this->assertEquals(3, iterator_count($container->getItemIterator()));
     }
 
 }
